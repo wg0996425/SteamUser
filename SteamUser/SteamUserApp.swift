412f2dadc452c7e7b735ae4b14df7cc10b408e8a
vg0996425 <wg0996425@otc.edu>
2026-04-27 08:48:20 -0500

    Initial Commit

---
+++ SteamUser/SteamUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamUserApp.swift
+//  SteamUser
+//
+//  Created by GRAY, VIKTOR on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SteamUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
